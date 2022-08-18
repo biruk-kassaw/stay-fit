c7c117131440bcf100873ab771c3ade2ca93bd00
biruk-kassaw <se.biruk.kassaw@gmail.com>
2022-08-18 23:15:20 +0300

    App Setup

--- App.tsx
+++ App.tsx
@@ -1,4 +1,9 @@
 import * as React from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './style.css';
 
 export default function App() {
