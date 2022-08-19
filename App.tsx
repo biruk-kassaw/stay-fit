d9315c95f98690bac4ab4972aaaa6ab0fab35a18
biruk-kassaw <se.biruk.kassaw@gmail.com>
2022-08-19 17:14:07 +0300

    Adding the home page

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
 
 import './style.css';
 
@@ -10,7 +19,20 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
     </div>
   );
 }
+
+const App = () => (
+  
+);
+
+export default App;
